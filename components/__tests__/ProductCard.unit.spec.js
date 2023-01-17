780f7a3dd10730b707bfcbfb9235e1b670064617
Vanessa Custódio <van.custodio.vc@gmail.com>
2023-01-17 14:42:08 -0300

    feat: add unit test productCard

---
+++ components/__tests__/ProductCard.unit.spec.js
@@ -0,0 +1,13 @@
+import { mount } from '@vue/test-utils';
+import ProductCard from '@/components/ProductCard';
+describe('ProductCard - unit', () => {
+  it('should mount the component', () => {
+    const wrapper = mount(ProductCard, {
+      propsData: {
+        product: {},
+      },
+    });
+    expect(wrapper.vm).toBeDefined();
+    console.log(wrapper.classes()); // exibe as classes usadas no componente
+  });
+});
