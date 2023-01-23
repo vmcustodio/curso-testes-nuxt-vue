9f5934f03b499b3e5683b46a9713f298761d6f48
Vanessa Custódio <van.custodio.vc@gmail.com>
2023-01-23 12:10:54 -0300

    feat: add tests products total

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,11 @@
     <search @doSearch="setSearchTerm" />
     <div v-if="errorMessage === ''" class="container mx-auto px-6">
       <h3 class="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
-      <span class="mt-3 text-sm text-gray-500">200+ Products</span>
+      <span
+        data-testid="total-quantity-label"
+        class="mt-3 text-sm text-gray-500"
+        >{{ quantityLabel }}</span
+      >
       <div
         class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
       >
@@ -14,7 +18,7 @@
         />
       </div>
     </div>
-    <h3 v-else class="text-center text-2x1">{{ errorMessage }}</h3>
+    <h3 v-else class="text-center text-2xl">{{ errorMessage }}</h3>
   </main>
 </template>
 
@@ -39,6 +43,13 @@ export default {
         });
       }
       return this.products;
+    },
+    quantityLabel() {
+      const {
+        list: { length },
+      } = this;
+
+      return length === 1 ? `${length} Product` : `${length} Products`;
     },
   },
   async created() {
